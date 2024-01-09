ffb9c88d0e462fd1c66d7008f103eb3bdb473459
rahullokhande20 <rahullokhande20@gmail.com>
2024-01-09 19:36:30 +0530

    Initial Commit

---
+++ MainApp/MainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainAppApp.swift
+//  MainApp
+//
+//  Created by Rahul Lokhande on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
